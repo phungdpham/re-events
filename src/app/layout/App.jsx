afbe0001abcba705973c6a572507cec89f9f4078
Phung Pham <phung.tech@gmail.com>
2020-02-23 18:43:47 -0600

    first git

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from "react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import Navbar from "../../features/nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment  >
+        <Navbar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
